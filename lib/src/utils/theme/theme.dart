85bfff794d8138f8c7a5432c10f5c3f3026a1fa5
miiandyuu <ric.aston777@gmail.com>
2023-02-10 17:48:52 +0700

    add splash screen

---
+++ lib/src/utils/theme/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(brightness: Brightness.light);
+  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+}
